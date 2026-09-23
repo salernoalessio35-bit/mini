14271523c198d02f7eea3cfd81931831791fdffd
salernoalessio35-bit <salernoalessio35@gmail.com>
2026-09-23 16:17:44 +0200

    Fix live transcription on iOS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mini-v21";
+const CACHE_NAME = "mini-v22";
 const APP_FILES = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
